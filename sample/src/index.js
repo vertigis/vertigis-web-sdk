27669c4639a6818a574baab5e3f0de0df60ea159
Ian Schmitz <ischmitz@latitudegeo.com>
2019-06-25 20:51:03 +0000

    Add React Dev Tools support

--- sample/src/index.js
+++ sample/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-// import { LayoutElement } from "@geocortex/web-viewer!framework/components";
+import { LayoutElement } from "@geocortex/web-viewer!/framework/components";
 
 export function Test(props) {
     return (
-        // <LayoutElement {...props}>
-        <img src="https://www.reactiongifs.us/wp-content/uploads/2013/07/its_working_star_wars.gif" />
-        // </LayoutElement>
+        <LayoutElement {...props}>
+            <div>
+                <img src="https://www.reactiongifs.us/wp-content/uploads/2013/07/its_working_star_wars.gif" />
+            </div>
+        </LayoutElement>
     );
 }
