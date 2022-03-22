69c0633cd0fdf341286d81838a669db1316b3206
Eric Parsons <eric.d.parsons@gmail.com>
2022-03-22 15:17:43 -0700

    feat: Adding a script for updating package.json dev dependencies (#33)
    * Adding a script for updating package.json dev dependencies.
    
    * Updating README to include upgrade script.

---
+++ scripts/upgrade.js
@@ -0,0 +1,62 @@
+"use strict";
+
+// This script will update the custom library to target the latest version of
+// VertiGIS Studio Web and the Web SDK.
+
+const fetch = require("node-fetch");
+const fs = require("fs");
+const spawn = require("cross-spawn");
+
+(async () => {
+    console.info("Determining latest versions of Web and Web SDK...");
+    let responses = await Promise.all([
+        fetch("https://registry.npmjs.com/@vertigis/web/"),
+        fetch("https://registry.npmjs.com/@vertigis/web-sdk/"),
+    ]);
+    const [webInfo, sdkInfo] = await Promise.all(
+        responses.map((r) => r.json())
+    );
+    const latestWeb = webInfo["dist-tags"]?.latest;
+    if (!latestWeb) {
+        throw new Error(
+            "Unable to determine the latest version VertiGIS Studio Web."
+        );
+    }
+    const latestSDK = sdkInfo["dist-tags"]?.latest;
+    if (!latestSDK) {
+        throw new Error(
+            "Unable to determine the latest version VertiGIS Studio Web SDK."
+        );
+    }
+
+    const projectPackage = JSON.parse(
+        await fs.promises.readFile("package.json", "utf8")
+    );
+
+    // Update Web and SDK to latest versions.
+    projectPackage.devDependencies["@vertigis/web"] = `^${latestWeb}`;
+    projectPackage.devDependencies["@vertigis/web-sdk"] = `^${latestSDK}`;
+
+    // Add or update all of Web's dependencies as dev dependencies of this
+    // project. Although this is not strictly necessary for the project to
+    // build, aspects of VS Code intellisense like auto imports will not
+    // work without this.
+    console.info("Determining Web dependencies...");
+    const response = await fetch(
+        `https://registry.npmjs.com/@vertigis/web/${latestWeb}`
+    );
+    const webPackage = await response.json();
+    for (const [dep, version] of Object.entries(webPackage.dependencies)) {
+        projectPackage.devDependencies[dep] = version;
+    }
+
+    console.info("Updating package.json...");
+    await fs.promises.writeFile(
+        "package.json",
+        JSON.stringify(projectPackage, undefined, 4),
+        "utf8"
+    );
+
+    console.info("Running npm install...");
+    spawn.sync("npm", ["install"]);
+})();
