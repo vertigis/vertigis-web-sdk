dc6d15efc5350fd598fe4e1651148f8a444dca17
Eric Parsons <eric.parsons@vertigis.com>
2022-11-25 10:24:49 -0600

    Fix for wrong npm version.

--- scripts/create.js
+++ scripts/create.js
@@ -23,38 +23,38 @@ const checkSpawnSyncResult = syncResult => {
 };
 
 /**
- * @param {string} rootPath
+ * @param {string} projectPath
  */
-const copyTemplate = rootPath => {
-    if (fs.existsSync(rootPath) && fs.readdirSync(rootPath).length > 0) {
+const copyTemplate = projectPath => {
+    if (fs.existsSync(projectPath) && fs.readdirSync(projectPath).length > 0) {
         console.error(
             chalk.red(
-                `Cannot create new project at ${chalk.green(rootPath)} as it already exists.\n`
+                `Cannot create new project at ${chalk.green(projectPath)} as it already exists.\n`
             )
         );
         process.exit(1);
     }
 
-    console.log(`Creating new project at ${chalk.green(rootPath)}`);
+    console.log(`Creating new project at ${chalk.green(projectPath)}`);
 
-    copySync(path.join(rootDir, "template"), rootPath, {
+    copySync(path.join(rootDir, "template"), projectPath, {
         errorOnExist: true,
         overwrite: false,
     });
     // Rename gitignore after the fact to prevent npm from renaming it to .npmignore
     // See: https://github.com/npm/npm/issues/1862
-    moveSync(path.join(rootPath, "gitignore"), path.join(rootPath, ".gitignore"));
+    moveSync(path.join(projectPath, "gitignore"), path.join(projectPath, ".gitignore"));
 };
 
 /**
- * @param {string} rootPath
+ * @param {string} projectPath
  */
-const updateTemplateContent = rootPath => {
+const updateTemplateContent = projectPath => {
     const randomNamespace = `custom.${crypto.randomBytes(4).toString("hex")}`;
 
     const filesToUpdate = [
-        path.join(rootPath, "app/layout.xml"),
-        path.join(rootPath, "src/index.ts"),
+        path.join(projectPath, "app/layout.xml"),
+        path.join(projectPath, "src/index.ts"),
     ];
 
     for (const fileToUpdate of filesToUpdate) {
@@ -64,7 +64,7 @@ const updateTemplateContent = rootPath => {
     }
 };
 
-const installNpmDeps = rootPath => {
+const installNpmDeps = projectPath => {
     console.log(`Installing packages. This might take a couple minutes.\n`);
     /**
      * @type {string}
@@ -75,44 +75,63 @@ const installNpmDeps = rootPath => {
         })
     ).version;
 
-    // First install existing deps.
-    checkSpawnSyncResult(
-        spawn.sync("npm", ["install"], {
-            cwd: rootPath,
-            stdio: "inherit",
-        })
+    // One of this project's dependencies (semantic-release) has "npm" as a
+    // dependency. This puts a specific version of the npm binaries in the
+    // node_modules/.bin folder. Unfortunately, this means that running "npm"
+    // below will pick up this version instead of whatever is installed on the
+    // user's machine. This can have unintended side-effects, such as generating
+    // a lock file with the wrong version. Temporarily rename the whole folder
+    // to avoid this.
+    fs.renameSync(
+        path.join(rootDir, "node_modules", ".bin"),
+        path.join(rootDir, "node_modules", ".bin.ignore")
     );
 
-    // Add SDK and Web runtime packages.
-    checkSpawnSyncResult(
-        spawn.sync(
-            "npm",
-            [
-                "install",
-                "--save-dev",
-                "--save-exact",
-                process.env.SDK_LOCAL_DEV === "true"
-                    ? process.cwd()
-                    : `@vertigis/web-sdk@${selfVersion}`,
-                "@vertigis/web",
-            ],
-            {
-                cwd: rootPath,
+    try {
+        // First install existing deps.
+        checkSpawnSyncResult(
+            spawn.sync("npm", ["install"], {
+                cwd: projectPath,
                 stdio: "inherit",
-            }
-        )
-    );
+            })
+        );
+
+        // Add SDK and Web runtime packages.
+        checkSpawnSyncResult(
+            spawn.sync(
+                "npm",
+                [
+                    "install",
+                    "--save-dev",
+                    "--save-exact",
+                    process.env.SDK_LOCAL_DEV === "true"
+                        ? process.cwd()
+                        : `@vertigis/web-sdk@${selfVersion}`,
+                    "@vertigis/web",
+                ],
+                {
+                    cwd: projectPath,
+                    stdio: "inherit",
+                }
+            )
+        );
+    } finally {
+        fs.renameSync(
+            path.join(rootDir, "node_modules", ".bin.ignore"),
+            path.join(rootDir, "node_modules", ".bin")
+        );
+    }
 };
 
 /**
  * Initialize newly cloned directory as a git repo.
  *
- * @param {string} rootPath
+ * @param {string} projectPath
  */
-const gitInit = rootPath => {
-    console.log(`Initializing git in ${rootPath}\n`);
+const gitInit = projectPath => {
+    console.log(`Initializing git in ${projectPath}\n`);
 
-    spawn.sync(`git init`, { cwd: rootPath }).status;
+    spawn.sync(`git init`, { cwd: projectPath }).status;
 };
 
 const printSuccess = () => {
