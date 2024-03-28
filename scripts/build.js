761fcc1a019883daaf853c2f51220870ba248648
Yona Bystedt <yona.bystedt@vertigis.com>
2024-03-28 13:44:02 -0700

    misc fixes and upgrades

--- scripts/build.js
+++ scripts/build.js
@@ -61,7 +61,7 @@ const build = () => {
                 );
                 console.log(
                     `You can learn more about deploying your custom code at ${chalk.cyan(
-                        "https://developers.vertigis.com/docs/web/overview/"
+                        "https://developers.vertigisstudio.com/docs/web/overview/"
                     )}`
                 );
             }
