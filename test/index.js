ee0a89cb7e140f5a421645f99f0d3f98aa281a0d
Yona Bystedt <yona.bystedt@vertigis.com>
2025-10-21 09:21:11 -0700

    use sdk library tests

--- test/index.js
+++ test/index.js
@@ -1,126 +1,21 @@
 // @ts-check
 "use strict";
 
-process.env.OPEN_BROWSER = "false";
-process.env.SDK_LOCAL_DEV = "true";
-
-import { strict as assert } from "assert";
-import { execSync } from "child_process";
-import * as fs from "fs";
-import * as path from "path";
+import path from "path";
 import { fileURLToPath } from "url";
 
-import * as execa from "execa";
-import pRetry from "p-retry";
-import { chromium } from "playwright-chromium";
+import { $ } from "execa";
 
-const dirName = path.dirname(fileURLToPath(import.meta.url));
-const rootDir = path.join(dirName, "..");
-const testLibProjPath = path.join(rootDir, "test-lib");
+// Will be set on child processes (via execa).
+process.env.OPEN_BROWSER = "false";
+process.env.SDK_LOCAL_DEV = "true";
+process.env.ROOT_DIRECTORY = path.join(path.dirname(fileURLToPath(import.meta.url)), "..");
+process.env.TEST_PROJECT_PATH = path.join(process.env.ROOT_DIRECTORY, "test-lib");
+process.env.SMOKE_TEST = "true";
+process.env.SDK_PLATFORM = "web";
 
-/**
- * @type {execa.ExecaChildProcess<string> | undefined}
- */
-let subprocess;
-
-/**
- * @param {Array<string>} args
- * @param {execa.Options} [opts]
- */
-function runNpmScript(args, opts) {
-    console.log(`Executing CLI script: ${args.join(" ")}`);
-    const scriptProcess = execa.execaNode(
-        path.join(rootDir, "bin/vertigis-web-sdk.js"),
-        args,
-        opts
-    );
-
-    // Pipe process output to current process output so it is visible in the
-    // console, but still allows us to examine the subprocess stdout/stderr
-    // variables.
-    scriptProcess?.stdout?.pipe(process.stdout);
-    scriptProcess?.stderr?.pipe(process.stderr);
-
-    return scriptProcess;
-}
-
-function killSubprocess() {
-    if (subprocess != null && !subprocess.killed) {
-        subprocess.kill();
-        subprocess = undefined;
-    }
-}
-
-async function testCreateProject() {
-    // First try creating the project.
-    execSync("npm pack");
-    subprocess = runNpmScript(["create", "test-lib"]);
-    await subprocess;
-
-    // Try to create same named project again.
-    subprocess = runNpmScript(["create", "test-lib"], { reject: false });
-    const processResult = await subprocess;
-    assert.strictEqual(
-        processResult.stderr.includes(
-            `Cannot create new project at ${testLibProjPath} as it already exists`
-        ),
-        true,
-        "Failed to detect existing directory"
-    );
-}
-
-// We assume the project was successfully created to run the following tests.
-async function testBuildProject() {
-    subprocess = runNpmScript(["build"], { cwd: testLibProjPath });
-    await subprocess;
-    assert.strictEqual(
-        fs.existsSync(path.join(testLibProjPath, "build/main.js")),
-        true,
-        "build/main.js is missing"
-    );
-}
-
-async function testStartProject() {
-    subprocess = runNpmScript(["start"], { cwd: testLibProjPath });
-
-    const browser = await chromium.launch();
-
-    try {
-        const page = await browser.newPage();
-        await pRetry(() => page.goto("http://localhost:3001"), {
-            maxRetryTime: 10000,
-        });
-        const frame = page.frame("viewer");
-        await frame?.waitForSelector("text=Points of Interest");
-    } finally {
-        await browser.close();
-        killSubprocess();
-    }
-}
-
-/**
- * @param {fs.PathLike} path
- */
-function rmdir(path) {
-    fs.rmdirSync(path, { recursive: true });
-}
-
-function cleanup() {
-    console.log("Cleaning up...");
-    killSubprocess();
-    rmdir(testLibProjPath);
-    console.log("Done cleaning.");
-}
-
-try {
-    await testCreateProject();
-    await testBuildProject();
-    await testStartProject();
-    console.log("All tests passed!");
-    cleanup();
-} catch (error) {
-    console.error("Test failed.");
-    console.error(error);
-    cleanup();
-    process.exit(1);
-}
+await $({
+    node: true,
+    stderr: "inherit",
+    stdout: "inherit",
+})`node_modules/@vertigis/sdk-library/test/e2e/tests.js`;
