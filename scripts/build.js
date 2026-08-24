2984b722371ed77f7ff761f9c25dfa66153e6f6c
yonabystedt <156722650+yonabystedt@users.noreply.github.com>
2026-08-24 08:51:08 -0700

    feat: Add versioning information to the build. (#77)
    After creating and building a new project you should now see a banner comment at the top of build/main.js similar to the following:
    
    /*!
     * library id 1ac78f66e81b0e48
     * web v5.42.0
     * web-sdk v0.0.0-semantically-released
     * sdk-library v2.1.0
     */

--- scripts/build.js
+++ scripts/build.js
@@ -6,12 +6,22 @@ import path from "node:path";
 import { pathToFileURL } from "node:url";
 
 import paths from "@vertigis/sdk-library/config/paths.js";
+import libPackage from "@vertigis/sdk-library/package.json" with { type: "json" };
 import sdkBuild from "@vertigis/sdk-library/scripts/build.js";
+import webPackage from "@vertigis/web/package.json" with { type: "json" };
+
+import sdkPackage from "../package.json" with { type: "json" };
 
 // These needs to be set prior to importing the webpack config. The only way to
 // do that with ES modules is by using a dynamic import.
 process.env.BABEL_ENV = "production";
 process.env.NODE_ENV = "production";
+
+// This information will be included in a banner comment.
+process.env.SDK_PLATFORM = "web";
+process.env.SDK_VERSION = sdkPackage.version;
+process.env.SDK_LIBRARY_VERSION = libPackage.version;
+process.env.PLATFORM_VERSION = webPackage.version;
 
 // Load the webpack.config.js from the project folder if it exists.
 const localWebPackPath = path.join(paths.projRoot, "webpack.config.js");
@@ -29,7 +39,7 @@ process.on("unhandledRejection", err => {
 });
 
 try {
-    await sdkBuild(webpackConfig, "web");
+    await sdkBuild(webpackConfig);
 } catch (e) {
     if (e instanceof Error && e.message) {
         console.error(e);
--- scripts/start.js
+++ scripts/start.js
@@ -6,12 +6,22 @@ import path from "node:path";
 import { pathToFileURL } from "node:url";
 
 import paths from "@vertigis/sdk-library/config/paths.js";
+import libPackage from "@vertigis/sdk-library/package.json" with { type: "json" };
 import sdkStart from "@vertigis/sdk-library/scripts/start.js";
+import webPackage from "@vertigis/web/package.json" with { type: "json" };
+
+import sdkPackage from "../package.json" with { type: "json" };
 
 // These needs to be set prior to importing the webpack config. The only way to
 // do that with ES modules is by using a dynamic import.
 process.env.BABEL_ENV = "development";
 process.env.NODE_ENV = "development";
+
+// This information will be included in a banner comment.
+process.env.SDK_PLATFORM = "web";
+process.env.SDK_VERSION = sdkPackage.version;
+process.env.SDK_LIBRARY_VERSION = libPackage.version;
+process.env.PLATFORM_VERSION = webPackage.version;
 
 // Load the webpack.config.js from the project folder if it exists.
 const localWebPackPath = path.join(paths.projRoot, "webpack.config.js");
